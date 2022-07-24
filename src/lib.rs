98d17c9e923e9933912ef296fdcd5003f8d3c935
xiexianbin <me@xiexianbin.cn>
2022-07-24 13:06:10 +0800

    feature: like grep func
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use std::fs;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Config {
+    pub key: String,
+    pub path: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough args. Usage: demo key ./abc.md")
+        }
+
+        let key: String = args[1].clone();
+        let path:String = args[2].clone();  // 解决所有权问题
+        Ok(Config{key, path})
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("{:?}", config);
+    let contents: String = fs::read_to_string(config.path)?;  // ? 异常返回 Err
+    for line in grep(&config.key, &contents) {
+        println!("{}", line)
+    }
+    Ok(())
+}
+
+pub fn grep<'a>(key: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(key) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_grep() {
+        let key = "abc";
+        let contents = "\
+abcdefghui
+xyz";
+        assert_eq!(vec!["abcdefghui"], grep(key, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+use demo::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();  // env::args_os() -> OsString
+    // let c = Config::new(&args);
+    // match c {
+    //     Ok(config) => println!("{:?}, args[0]: {:?}", config, config.name),
+    //     Err(msg) => println!("{}", msg)
+    // }
+    let c = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Occur Error: {}", err);
+        process::exit(1);  // 使 println 的输出无 debug 信息
+    });
+
+    if let Err(e) = demo::run(c) {
+        eprintln!("Run Error: {}", e);
+        process::exit(1);
+    }
 }
